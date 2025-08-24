4a0511966897f15464f80c5a44eb31936e759bb4
yasser <yasseralhassan942@gmail.com>
2025-08-24 11:59:05 +0300

    .

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 const api = axios.create({
 
     baseURL: import.meta.env.VITE_API_URL || 'https://guba-sy.com/api',
-    // baseURL: import.meta.env.VITE_API_URL || 'http://192.168.1.109:8000/api',
+    //baseURL: import.meta.env.VITE_API_URL || 'http://192.168.1.109:8000/api',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
